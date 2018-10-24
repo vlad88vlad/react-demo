ab2218caafca98fb1016bad631c50316d0549b97
vlad <vlad88vlad@gmail.com>
2018-10-24 21:46:34 +0300

    initial commit1

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -12,7 +12,7 @@ export default class About extends Component {
 
     render() {
         return (
-            <div>1
+            <div>
                 coming soon
             </div>
         );
